0069001690fe57e34592ed5d9472167f336f7dbf
murathankarasu <murathankarasu@icloud.com>
2025-03-18 00:01:57 +0300

    Initial Commit

---
+++ Lori/LoriApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoriApp.swift
+//  Lori
+//
+//  Created by Murathan Karasu on 18.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct LoriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
